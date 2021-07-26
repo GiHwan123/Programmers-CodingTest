6c22137ae4d25c19054f44795c8596b5d768d501
Gihwan <lgh95m@naver.com>
2021-07-26 21:19:23 +0900

    2021-07-26

--- level2/_42584.java
+++ level2/_42584.java
@@ -2,43 +2,35 @@ package level2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Stack;
 
 public class _42584 {
 
 	public static void main(String[] args) {
-		int[] prices = {1, 2, 3, 2, 3};
+		int[] prices = {1, 2, 3, 2, 3 , 3 ,1};
 		int[] answer = solution(prices);
 		System.out.println(Arrays.toString(answer));
 	}
 
 	
 	public static int[] solution(int[] prices) {
-        int[] answer = {};
-        ArrayList<Integer> arr = new ArrayList<>();
-        
-        for(int i=0;i<prices.length;i++) {//이중 포문을 이용해 현재 값과 이후의 값들을 모두 비교해준다
-        	int cnt=0;
-        	
-        	for(int j=i+1;j<prices.length;j++) {//i+1 부터 시작해 i값을 기준으로 뒷배열 값들을 비교.
-        		if(j == prices.length) {//배열의 길이를 넘어갈 시 0을 저장 후 break
-        			cnt=0;
-        			break;
-        		}
-        		else if(prices[i]<=prices[j]){//현재 기준값인 prices[i]값보다 크거나 같은 값들의 길이를 count
-        			cnt++;
-        		}
-        		else {//기준값보다 작은 값이 발견될 경우 cnt 1 증가 후 반복문 중지
-        			cnt++;
-        			break;
-        		}
-        	}
-        	arr.add(cnt);//answer배열의 길이를 동적으로 할당하기 위해 ArrayList에 각 결과값들을 할당
-        	
+        Stack<Integer> beginIdxs = new Stack<>();
+        int i=0;
+        int[] terms = new int[prices.length];
+
+        beginIdxs.push(i);
+        for (i=1; i<prices.length; i++) {
+            while (!beginIdxs.empty() && prices[i] < prices[beginIdxs.peek()]) {
+                int beginIdx = beginIdxs.pop();
+                terms[beginIdx] = i - beginIdx;
+            }
+            beginIdxs.push(i);
         }
-        answer=new int[arr.size()];//ArrayList의 크기만큼 배열의 길이 할당
-        for(int i=0;i<answer.length;i++) {// 값을 answer 배열에 할당
-        	answer[i]=arr.get(i);
+        while (!beginIdxs.empty()) {
+            int beginIdx = beginIdxs.pop();
+            terms[beginIdx] = i - beginIdx - 1;
         }
-        return answer;
+
+        return terms;
     }
 }
--- level2/_42586.java
+++ level2/_42586.java
@@ -4,45 +4,47 @@ import java.util.*;
 public class _42586 {
 	
 	public static void main(String[] args) {
-		int[] progresses = {93,30,55};
-		int[] speeds = {1,30,5};
+		int[] progresses = {95, 90, 99, 99, 80, 99};
+		int[] speeds = {1, 1, 1, 1, 1, 1};
 		int[] answer = solution(progresses,speeds);
 		System.out.println(Arrays.toString(answer));
 	}
 	public static int[] solution(int[] progresses, int[] speeds) {
-        
-        ArrayList<Integer> arr1 = new ArrayList<>();
-        ArrayList<Integer> arr2 = new ArrayList<>();
-       //잔업을 완수하기까지 남은 일수를 계산하여 arr1에 삽입
-        for(int i=0;i<progresses.length;i++){
-            int num=0;
-            num = 100 - progresses[i];
-            if(num%speeds[i]!=0){
-                num=num/speeds[i] + 1;
-            }
-            else{num = num / speeds[i];}
-            arr1.add(num);
-        }
-        //남은일수가 계산된 arr1의 값을 이용하여 첫 값보다 작은값들은 모두 첫 값이 배포되는 날에 배포될 것이므로 해당 값들 모두 산출하여 arr2에 저장
-        for(int i=0;i<arr1.size();i++){
-            int cnt=1;
-            for(int j=i+1;j<arr1.size();j++){
-                if(arr1.get(i)>=arr1.get(j)){
-                    cnt++;
-                }
-                else{
-                    break;
-                }
-                
-            }
-            arr2.add(cnt);
-            i= i + cnt -1;
-        }
-        //arr2에 저장된 값들을 answer에 옮김.
-        int[] answer = new int[arr2.size()];
-        for(int i=0;i<arr2.size();i++){
-            answer[i]=arr2.get(i);
-        }
+
+		Queue<Integer> que = new LinkedList<>();
+		ArrayList<Integer> arr = new ArrayList<>();
+		for(int i=0;i<progresses.length;i++) {
+			
+			int a=100-progresses[i];
+			
+			if(a%speeds[i]!=0) {
+				que.add(a/speeds[i]+1);
+			}
+			else {
+				que.add(a/speeds[i]);
+			}
+			
+		}
+		
+		int cnt=1;
+		int peek=que.poll();
+		while(!que.isEmpty()) {
+			if(que.peek()<=peek) {
+				cnt++;
+				que.remove();
+			}
+			else {
+				arr.add(cnt);
+				peek=que.poll();
+				cnt=1;
+			}
+		}
+		arr.add(cnt);
+		int[] answer=new int[arr.size()];
+		for(int i=0;i<arr.size();i++) {
+			answer[i]=arr.get(i);
+		}
+		
         return answer;
     }
 
