fc88e12cd9a5ddbca2d468b274ceb2c3223ca780
Gihwan <lgh95m@naver.com>
2021-07-27 22:41:15 +0900

    42583 Can't Solve

--- level2/_42583.java
+++ level2/_42583.java
@@ -1,18 +1,55 @@
 package level2;
 
+import java.util.*;
+
 public class _42583 {
 
 	public static void main(String[] args) {
 		
-		int bridge_length = 2;
-		int weight = 10;
-		int[] truck_weights = {7,4,5,6};
+		int bridge_length = 100;
+		int weight = 100;
+		int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
 		int answer = solution(bridge_length,weight,truck_weights);
+		System.out.println(answer);
 		
 	}
 	
 	public static int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
+        Queue<Integer> q1 = new LinkedList<>();//
+        Queue<Integer> q2 = new LinkedList<>();//
+        
+        for(int i : truck_weights) {
+        	q2.add(new Integer(i));
+        }
+        
+        q1.add(q2.poll());
+        while(!q1.isEmpty()) {
+        	//1초 단위로 상황을 체크 (각 트럭이 머무를 수 있는 시간은 무조건 2초 겹치는 조건을 처리해주어야함)
+        	int num=q1.peek();
+        	for(int i=0;i<bridge_length;i++) {
+        		answer++;
+        		if(!q2.isEmpty() && num + q2.peek() > weight) {
+        			continue;
+        		}
+        		else if(!q2.isEmpty() && num + q2.peek() <=weight) {
+        			num += q2.peek();
+        			q1.add(q2.poll());
+        			break;
+        		}
+        		else if(q2.isEmpty()) {
+        			continue;
+        		}
+        	}
+        	num = num - q1.peek();
+        	q1.remove();
+        	
+        	if(!q2.isEmpty() && num + q2.peek() <weight) {
+        		q1.add(q2.poll());
+        	}
+        	if(q1.isEmpty() && q2.isEmpty())
+        		answer++;
+        }
         return answer;
     }
 }
--- level2/_42586.java
+++ level2/_42586.java
@@ -11,36 +11,43 @@ public class _42586 {
 	}
 	public static int[] solution(int[] progresses, int[] speeds) {
 
-		Queue<Integer> que = new LinkedList<>();
-		ArrayList<Integer> arr = new ArrayList<>();
+		Queue<Integer> que = new LinkedList<>();//큐 선언
+		ArrayList<Integer> arr = new ArrayList<>();//정답 배열의 크기를 결정할 ArrayList
+		
 		for(int i=0;i<progresses.length;i++) {
 			
-			int a=100-progresses[i];
+			int a=100-progresses[i];//100 - 현재 개발 완성도를 변수값에 저장(남은잔업량)
 			
-			if(a%speeds[i]!=0) {
-				que.add(a/speeds[i]+1);
+			if(a%speeds[i]!=0) {//남은잔업량%개발진행속도의 값이 0 이 아닐경우(나머지가 존재할경우) 남은 일수 + 1
+				que.add(a/speeds[i]+1);//큐에 작업완료까지 남은 일수 저장
 			}
-			else {
+			else {//몫이 나누어 떨어질 경우 해당 값을 큐에 저장
 				que.add(a/speeds[i]);
 			}
 			
 		}
 		
 		int cnt=1;
-		int peek=que.poll();
-		while(!que.isEmpty()) {
-			if(que.peek()<=peek) {
+		int peek=que.poll();//큐에 처음 저장된 값 변수에 저장
+		
+		while(!que.isEmpty()) {//큐에 값이 존재하지 않을 때 까지 반복
+			
+			if(que.peek()<=peek) {//큐에서 뽑아낸 값보다 현재 큐 내에 존재하는 첫 값이 작을 경우 해당 값 삭제 후 cnt++
 				cnt++;
 				que.remove();
 			}
-			else {
+			
+			else {//큐에서 뽑아낸 값보다 현재 큐 내에 존재하는 첫 값이 큰 경우 cnt값을 arraylist에 저장 후 큐 내의 첫 값을
+				//peek 값에 저장하고 cnt 초기화
 				arr.add(cnt);
 				peek=que.poll();
 				cnt=1;
 			}
 		}
-		arr.add(cnt);
-		int[] answer=new int[arr.size()];
+		arr.add(cnt);//큐에 저장된 값이 없을 경우 마지막 cnt 값을 arr에 저장하지 않고 while문을 탈출하기 때문에 최종으로 arr에 cnt값
+		//1회 저장 명령 수행
+		
+		int[] answer=new int[arr.size()];//arr의 값을 answer 배열에 동일하게 할당
 		for(int i=0;i<arr.size();i++) {
 			answer[i]=arr.get(i);
 		}
